d0c6e46b35760c332605861b7716e855a68e87f0
JulianMunguia04 <julianmunguia04@gmail.com>
2025-02-19 14:36:36 -0700

    Finish Homepage

--- routes/recipe.js
+++ routes/recipe.js
@@ -12,8 +12,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/users', {
 .then(() => console.log('MongoDB connected successfully!'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
-router.get('/', (req, res) => {
-  res.render('recipes')  
+router.get('/', async (req, res) => {
+  const recipes = await Recipe.find()
+  res.send(recipes)
 })
 
 router.get('/:id', async (req, res) =>{
